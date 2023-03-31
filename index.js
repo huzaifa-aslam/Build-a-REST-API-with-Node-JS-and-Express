81e2099d08ad3902bb9ee4dfc4a6b5093ed2ef89
Huzaifa Aslam <huzaifaslam45@gmail.com>
2023-04-01 01:13:39 +0500

    add route

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 5000;
 app.use(bodyParser.json());
-
+app.get("/api/v1/users", (req, res) => {
+  res.status(200).json([
+    { id: 1, name: "ali" },
+    { id: 2, name: "hassan" },
+  ]);
+});
 app.listen(PORT, () => {
   console.log(`Server is Running on port http://localhost:${PORT}`);
 });
